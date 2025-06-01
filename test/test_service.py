98f572716bfec4c7883c0c396a18f06d4399c569
Dantee0 <d.illesca@alumno.um.edu.ar>
2025-06-01 20:07:26 -0300

    test_service updated

--- test/test_service.py
+++ test/test_service.py
@@ -1,51 +1,40 @@
 import unittest
-from unittest.mock import Mock, patch
-from flask import current_app
-from app.models import Product
+from unittest.mock import patch, Mock
+from app import create_app
 from app.services import ProductService
 
-from app import create_app, db
-
-class ProductTestCase(unittest.TestCase):
+class ProductServiceTestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = create_app()
+        self.client = self.app.test_client()
         self.app_context = self.app.app_context()
         self.app_context.push()
-        db.create_all()
+        self.mock_product = Mock()
+        self.mock_product.name = 'Coca Cola'
+        self.mock_product.bar_code = '77912'
+        self.mock_product.price = 4000.0
 
-    def tearDown(self) -> None:
-        db.session.remove()
-        db.drop_all()
-        self.app_context.pop()
 
-    @patch('app.services.ProductService')
-    def test_find_by_barcode(self, mock_service):
-        mock_product = Mock()
-        mock_product.name = 'Coca Cola'
-        mock_product.bar_code = '77912'
-        mock_product.price = 4000.0
-        mock_service.find_by_bar_code.return_value = mock_product
-
-        product = mock_service.find_by_bar_code('77912')
+    @patch('app.repositories.product_repository.ProductRepository.find_by_bar_code')
+    def test_find_by_barcode(self, mock_find_by_bar_code):
+        mock_find_by_bar_code.return_value = self.mock_product
+        service = ProductService()
+        product = service.find_by_bar_code('77912')
         self.assertEqual(product.name, 'Coca Cola')
         self.assertEqual(product.bar_code, '77912')
         self.assertEqual(product.price, 4000.0)
 
-
-    @patch('app.services.ProductService')
-    def test_find_by_name(self, mock_service):
-        mock_product = Mock()
-        mock_product.name = 'Coca Cola'
-        mock_product.bar_code = '77912'
-        mock_product.price = 4000.0
-        mock_service.find_by_name.return_value = [mock_product]
-
-        product = mock_service.find_by_name('Coca Cola')
-        self.assertEqual(product[0].name, 'Coca Cola')
-        self.assertEqual(product[0].bar_code, '77912')
-        self.assertEqual(product[0].price, 4000.0)
-
+    @patch('app.repositories.product_repository.ProductRepository.find_by_name')
+    def test_find_by_name(self, mock_find_by_name):
+        mock_find_by_name.return_value = [self.mock_product]
+        service = ProductService()
+        result = service.find_by_name('Coca')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].name, 'Coca Cola')
+        self.assertEqual(result[0].bar_code, '77912')
+        self.assertEqual(result[0].price, 4000.0)
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
+
