1e42cf073abcf021177527d810dc4d3ff493e40e
Tote1958 <s.beltran@alumno.um.edu.ar>
2025-06-01 17:48:48 -0300

    fixed tests get by id of controllers

--- app/controllers/product_controller.py
+++ app/controllers/product_controller.py
@@ -14,15 +14,20 @@ def get_all():
 def get_by_id(id):
     product = service.find_by_id(id)
     if not product:
-        return jsonify({'message': 'Producto no encontrado'}), 404
-    return jsonify(product_schema.dump(product))
+        response = jsonify({'message': 'Producto no encontrado'})
+        response.status_code = 404
+        return response
+    response = product_schema.dump(product)
+    response = jsonify(response)
+    response.status_code = 200
+    return response
 
 @product.route('/products/name/<string:name>', methods=['GET'])
 def get_by_name(name):
     products = service.find_by_name(name)
     if not products:
         return jsonify({'message': 'No se encontraron productos con ese nombre'}), 404
-    return jsonify(products_schema.dump(products))
+    return jsonify(products_schema.dump(products), 200)
 
 @product.route('/products/barcode/<string:bar_code>', methods=['GET'])
 def get_by_bar_code(bar_code):
--- test/test_controller.py
+++ test/test_controller.py
@@ -1,52 +1,31 @@
 import unittest
 from unittest.mock import patch
-from flask import Flask, jsonify, Blueprint
+from app import create_app
 
 class ProductControllerTestCase(unittest.TestCase):
 
-    # MOCKEO DE LA APP COMPLETA---------------------------------------------------------------------
+    # Set Up de la aplicacion y el cliente de prueba, creando un producto Mock
     def setUp(self):
-        self.app = Flask(__name__)
-        self.app.testing = True
-
-        product = Blueprint('product', __name__)
-
-        @product.route('/products/<int:id>', methods=['GET'])
-        def get_by_id(id):
-            from app.controllers import product_controller
-            product = product_controller.service.find_by_id(id)
-            if not product:
-                return jsonify({'message': 'Producto no encontrado'}), 404
-            return jsonify(product_controller.product_schema.dump(product))
-
-        @product.route('/products/bar_code/<bar_code>', methods=['GET'])
-        def get_by_bar_code(bar_code):
-            from app.controllers import product_controller
-            product = product_controller.service.find_by_bar_code(bar_code)
-            if not product:
-                return jsonify({'message': 'Producto no encontrado'}), 404
-            return jsonify(product_controller.product_schema.dump(product))
-        
-        self.app.register_blueprint(product)
+        self.app = create_app()
         self.client = self.app.test_client()
-    # MOCKEO DE LA APP COMPLETA---------------------------------------------------------------------
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.mock_product = {'id': 1, 'name': 'Coca Cola','bar_code':'77912', 'price': 4000.0}
+        
 
     @patch('app.controllers.product_controller.service.find_by_id')
-    @patch('app.controllers.product_controller.product_schema.dump')
-    def test_get_by_id_success(self, mock_find_by_id, mock_dump):
-        mock_product = {'id': 1, 'name': 'Coca Cola','bar_code':'77912', 'price': 4000.0}
-        mock_find_by_id.return_value = mock_product
-        mock_dump.return_value = mock_product
-
-        response = self.client.get('/products/1')
+    def test_get_by_id_success(self, mock_find_by_id):
+        mock_find_by_id.return_value = self.mock_product
+        
+        response = self.client.get('/api/v1/products/1')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.get_json(), mock_product)
+        self.assertEqual(response.get_json(), self.mock_product)
 
     @patch('app.controllers.product_controller.service.find_by_id')
     def test_get_by_id_not_found(self, mock_find_by_id):
         mock_find_by_id.return_value = None
 
-        response = self.client.get('/products/999')
+        response = self.client.get('/api/v1/products/999')
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.get_json(), {'message': 'Producto no encontrado'})
 
